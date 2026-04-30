9117121890c89e52debcb5b93a9e79f17b1f5dd1
Jakub Houser <jakub.houser@proton.me>
2026-04-30 10:55:35 +0200

    Add Sanity Studio and post schema

---
+++ studio/schemaTypes/post.js
@@ -0,0 +1,57 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: { type: 'category' },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+    {
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+      description: 'A short summary of the post used in the blog list.',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'Quote/Callout', value: 'blockquote'}
+          ],
+        }
+      ],
+    },
+  ],
+  initialValue: () => ({
+    publishedAt: (new Date()).toISOString()
+  })
+}
